c84a73825f3f48e9fa98291a97e4cc21b56202ac
VStefanov89 <vstefanovofm@gmail.com>
2022-08-01 16:35:34 +0300

    Creating test for append method

--- unit_test/tests.py
+++ unit_test/tests.py
@@ -1,8 +1,14 @@
 from unittest import TestCase, main
+from custom_list.lists import CustomList
+
 
 class CustomListTest(TestCase):
-    def
+    def test_append_method_and_return_list(self):
+        my_list = CustomList(1, 2, 3)
+        self.assertEqual([1, 2, 3], my_list._CustomList__elements)
 
+        my_list.append(4)
+        self.assertEqual([1, 2, 3, 4], my_list._CustomList__elements)
 
 
 if __name__ == '__main__':
