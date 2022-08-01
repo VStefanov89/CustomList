3ad3ea6b9a80b25522806bf70210a4a2c6361139
VStefanov89 <vstefanovofm@gmail.com>
2022-08-01 16:35:03 +0300

    Creating append method

--- custom_list/lists.py
+++ custom_list/lists.py
@@ -4,7 +4,13 @@ class CustomList:
         for el in args:
             self.__elements.append(el)
 
+    def append(self, value):
+        self.__elements.append(value)
+
     def __str__(self):
         result = ", ".join([str(el) for el in self.__elements])
         return f"[{result}]"
 
+my_list = CustomList(1, 2, 3)
+
+print(my_list._CustomList__elements)
